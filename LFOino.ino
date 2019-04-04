5550150fec8aec956975fa3ea8d35bcaeb95f9b6
davide.magni@outlook.com <davide.magni@outlook.com>
2019-04-04 15:06:51 +0200

    * added correct timer + sr

--- LFOino.ino
+++ LFOino.ino
@@ -1,9 +1,11 @@
 #include <math.h> 
 #define LED_PIN 6
 #define POT_PIN A0
-#define BUTTON_PIN A1
-#define RATE_MIN 1.0
+#define BUTTON_PIN 5
+#define RATE_MIN 0.1
 #define RATE_MAX 10.0
+
+#define SR 32000.0
 
 double ph = 0;
 double brightness = 0;
@@ -11,56 +13,75 @@ double rate = 0;
 double value = 0;
 
 int wave = 0;
-int waveBtnState;
+bool waveBtnState;
+
+
 
 void setup() {
     pinMode(LED_PIN, OUTPUT);
     pinMode(POT_PIN, INPUT);
     pinMode(BUTTON_PIN, INPUT);
     Serial.begin(9600);
+    cli();
+
+    TCCR1A = 0;// set entire TCCR1A register to 0
+    TCCR1B = 0;// same for TCCR1B
+    TCNT1  = 0;//initialize counter value to 0
+    
+    OCR1A = (16000000/SR)-1;
+    // turn on CTC mode
+    TCCR1B |= (1 << WGM12);
+    // prescaler is 1
+    TCCR1B |= (1 << CS10);  
+    // enable timer compare interrupt
+    TIMSK1 |= (1 << OCIE1A);
+
+    sei();
+} 
+
+extern "C"
+{
+  ISR(TIMER1_COMPA_vect){
+    ph = ph + rate; 
+
+    if (ph >= SR) {
+      ph = 0;
+    };
+  }
 }
 
 void loop() {
-	rate = map(analogRead(POT_PIN), 0, 1023, RATE_MIN, RATE_MAX);
-	ph = ph + rate; 
+    rate = map(analogRead(POT_PIN), 0, 1023, RATE_MIN, RATE_MAX);
 
-	if (ph >= 1000) {
-		ph = 0;
-	};
-
-	if (analogRead(BUTTON_PIN) != waveBtnState && analogRead(BUTTON_PIN) == 1023) {
-		wave = (wave+1) % 4;
-	};
-	waveBtnState = analogRead(BUTTON_PIN);
+    if (digitalRead(BUTTON_PIN) == 1 && waveBtnState != digitalRead(BUTTON_PIN)) {
+      wave = (wave + 1) % 4;
+    };
+    waveBtnState = digitalRead(BUTTON_PIN);
 
     switch (wave) {
       case 0:
-		value = ((ph / 1000.0) * 2) - 1;
+         value = ((ph / SR) * 2) - 1;
         break;
 
       case 1:
-		value = sin((ph / 1000.0) * 2*PI);
+        value = sin((ph / SR) * 2*PI);
         break;
 
       case 2:
-      	if (ph < 500) {
-      		value = 1.0;
-      	}
-      	else {
-      		value = -1.0;
-      	};
+        if (ph < SR/2) {
+          value = 1.0;
+        }
+        else {
+          value = -1.0;
+        };
         break;
 
       case 3:
-      	value = ph - 500;
-      	value = (0.002 * abs(value) * 2) - 1;
+        value = ph - SR/2;
+        value = (0.002 * abs(value) * 2) - 1;
         break;
     }
 
-
-	brightness = ((value / 2.0) + 0.5) * 255;
-    Serial.println(brightness);
-	analogWrite(LED_PIN, brightness);
-
-	delayMicroseconds(1000);
+    brightness = ((value / 2.0) + 0.5) * 255;
+    analogWrite(LED_PIN, brightness);
 } 
